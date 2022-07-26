2ae00a1f3d6f59caa333d88b484802db780baa77
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-26 15:46:07 +0900

    feat: CoffeeTest class including main

--- Week1/src/ex02/CoffeeTest.java
+++ Week1/src/ex02/CoffeeTest.java
@@ -1,2 +1,23 @@
-package ex02;public class CoffeeTest {
+package ex02;
+
+public class CoffeeTest {
+    public static void main(String[] args) {
+        Coffee kenyaAmericano = new KenyaAmericano();
+        kenyaAmericano.brewing();
+        System.out.println();
+
+        Coffee kenyaLatte = new Latte(kenyaAmericano);
+        kenyaLatte.brewing();
+        System.out.println();
+
+        Mocha kenyaMocha = new Mocha(new Latte(new KenyaAmericano()));
+        kenyaMocha.brewing();
+        System.out.println();
+
+        WhippedCream etiopiaWhippedMocha =
+                new WhippedCream(new Mocha(new Latte(new EtiopiaAmericano())));
+        etiopiaWhippedMocha.brewing();
+        System.out.println();
+    }
 }
+
