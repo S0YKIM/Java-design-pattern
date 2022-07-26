18468786dce7e675db0b4bf0a1d0df25c9e2641e
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-26 15:45:01 +0900

    feat: Concrete classes inheriting Decorator class

--- Week1/src/ex02/Mocha.java
+++ Week1/src/ex02/Mocha.java
@@ -1,2 +1,15 @@
-package ex02;public class Mocha {
+package ex02;
+
+public class Mocha extends Decorator{
+
+    public Mocha(Coffee coffee) {
+        super(coffee);
+        // TODO Auto-generated constructor stub
+    }
+
+    public void brewing() {
+        super.brewing();
+        System.out.print("Adding Mocha Syrup ");
+    }
 }
+
--- Week1/src/ex02/WhippedCream.java
+++ Week1/src/ex02/WhippedCream.java
@@ -1,2 +1,14 @@
-package ex02;public class WhippedCream {
+package ex02;
+
+public class WhippedCream extends Decorator{
+
+    public WhippedCream(Coffee coffee) {
+        super(coffee);
+    }
+
+    public void brewing() {
+        super.brewing();
+        System.out.print("Adding WhippedCream ");
+    }
 }
+
