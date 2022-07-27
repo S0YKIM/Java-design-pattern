58db9b31808c466f67d2d00c31010d7e56ee863c
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-27 18:38:51 +0900

    add: Problem set from subject

--- Week2/src/ex00/MainBoard.java
+++ Week2/src/ex00/MainBoard.java
@@ -1,2 +1,12 @@
-package ex00;public class Mainboard {
+package ex00;
+
+public class MainBoard {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.play(1);
+        player.upgradeLevel(Player.ADVANCED_LEVEL);
+        player.play(2);
+        player.upgradeLevel(Player.SUPER_LEVEL);
+        player.play(3);
+    }
 }
