4a8a8716607602bd1a13edd336bb82edf9442229
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-30 16:58:37 +0900

    feat: Player class which has PlayerLevel class as private property

--- Week2/src/ex00/Player.java
+++ Week2/src/ex00/Player.java
@@ -1,2 +1,19 @@
-package ex00;public class Player {
+package ex00;
+
+public class Player {
+    private PlayerLevel level;
+    public Player() {
+        level = new BeginnerLevel();
+        level.showLevelMessage();
+    }
+    // setter
+    public void upgradeLevel(PlayerLevel level) {
+        this.level = level;
+        level.showLevelMessage();
+    }
+    // getter
+    public PlayerLevel  getLevel() { return level; }
+    public void play(int time) {
+        level.go(time);
+    }
 }
