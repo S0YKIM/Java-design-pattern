ba4059a36c0d6728cdae2313ec79e947fddb6eb1
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-30 17:00:20 +0900

    feat: SuperLevel concrete class

--- Week2/src/ex00/SuperLevel.java
+++ Week2/src/ex00/SuperLevel.java
@@ -1,2 +1,19 @@
-package ex00;public class SuperLevel {
+package ex00;
+
+public class SuperLevel extends PlayerLevel{
+    public void jump() {
+        System.out.println("아주 높이 jump 합니다.");
+    }
+
+    public void run() {
+        System.out.println("엄청 빨리 달립니다.");
+    }
+
+    public void turn() {
+        System.out.println("한 바퀴 돕니다.");
+    }
+
+    public void showLevelMessage() {
+        System.out.println("현재 플레이어의 레벨은 고급자입니다.");
+    }
 }
