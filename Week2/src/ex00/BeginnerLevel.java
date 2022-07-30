07a8dff32a9deef6ebb8a622e33b10d7a97f18cc
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-30 16:59:51 +0900

    feat: BeginnerLevel concrete class

--- Week2/src/ex00/BeginnerLevel.java
+++ Week2/src/ex00/BeginnerLevel.java
@@ -1,2 +1,19 @@
-package ex00;public class BeginnerLevel {
+package ex00;
+
+public class BeginnerLevel extends PlayerLevel{
+    public void jump() {
+            System.out.println("Jump 할 줄 모르지롱.");
+    }
+
+    public void run() {
+            System.out.println("천천히 달립니다.");
+    }
+
+    public void turn() {
+            System.out.println("Turn 할 줄 모르지롱.");
+    }
+
+    public void showLevelMessage() {
+        System.out.println("현재 플레이어의 레벨은 초보자입니다.");
+    }
 }
