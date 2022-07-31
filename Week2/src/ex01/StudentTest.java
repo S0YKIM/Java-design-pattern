6092cbb9b61962d40aab072f5b7e38bf830d708d
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-31 17:25:21 +0900

    feat: Main statements

--- Week2/src/ex01/StudentTest.java
+++ Week2/src/ex01/StudentTest.java
@@ -12,7 +12,6 @@ public class StudentTest {
         studentKim.addSubject("영어", 100, false);
 
         studentLee.showGradeInfo();
-        System.out.println("======================================");
         studentKim.showGradeInfo();
     }
 }
