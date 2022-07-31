5ddbe7431b2f18d9b5e7a36d6ea0899b43816357
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-31 17:26:21 +0900

    feat: Concrete classes implementing interface

--- Week2/src/ex01/BasicStrategy.java
+++ Week2/src/ex01/BasicStrategy.java
@@ -1,2 +1,16 @@
-package ex01;public class BasicStrategy {
+package ex01;
+
+public class BasicStrategy implements GradeStrategy {
+    public void showGrade(String name, int score) {
+        if (score >= 90)
+            System.out.println(name + ": A");
+        else if (score >= 80)
+            System.out.println(name + ": B");
+        else if (score >= 70)
+            System.out.println(name + ": C");
+        else if (score >= 55)
+            System.out.println(name + ": D");
+        else
+            System.out.println(name + ": F");
+    }
 }
