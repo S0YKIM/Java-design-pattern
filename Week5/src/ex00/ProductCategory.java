d1d87122c41e18a169933b1f96717621fc0bf364
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-08-24 12:56:30 +0900

    feat: Component

--- Week5/src/ex00/ProductCategory.java
+++ Week5/src/ex00/ProductCategory.java
@@ -1,2 +1,19 @@
-package ex00;public class ProductCategory {
+package ex00;
+
+public abstract class ProductCategory {
+    int id;
+    String name;
+    int price;
+    public ProductCategory(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    public abstract void addProduct(ProductCategory product);
+    public abstract void removeProduct(ProductCategory product);
+    public abstract void displayProduct();
+    public abstract int getCount();
+    public abstract String getName();
+    public abstract int getPrice();
+    public abstract int getId();
 }
