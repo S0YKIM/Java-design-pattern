6028b3fa4e62626105838245a15807dc2fefd4fe
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-08-24 12:56:18 +0900

    feat: Leaf

--- Week5/src/ex00/Product.java
+++ Week5/src/ex00/Product.java
@@ -1,2 +1,40 @@
-package ex00;public class Product {
+package ex00;
+
+public class Product extends ProductCategory{
+    @Override
+    public int getCount() {
+        return 1;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public Product(int id, String name, int price) {
+        super(id, name, price);
+    }
+
+    @Override
+    public void addProduct(ProductCategory product) {
+    }
+
+    @Override
+    public void removeProduct(ProductCategory product) {
+    }
+
+    @Override
+    public void displayProduct() {
+    }
 }
+
